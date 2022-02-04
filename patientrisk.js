71b8de0d81f8eff00add4147d4e5b6f108140138
dsk5jy <63305103+dsk5jy@users.noreply.github.com>
2022-02-03 23:03:31 -0500

    Add files via upload

---
+++ patientrisk.js
@@ -0,0 +1,12 @@
+import { AdditionalNotesBox } from './ui-components';
+import { Group132 } from './ui-components';
+import { ViewAnalyticsDigitizedSheetsButton } from './ui-components';
+import { IssueResolved } from './ui-components';
+import { NeedsFollowUp } from './ui-components';
+import { Printeronitbutton } from './ui-components';
+
+
+
+<><><><><><><><><Signout /><AdditionalNotesBox />
+</></></><ViewAnalyticsDigitizedSheetsButton />
+</><IssueResolved /></><NeedsFollowUp /></><Printeronitbutton /></><Group132 /></>
